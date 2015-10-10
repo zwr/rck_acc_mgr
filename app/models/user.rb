4eeaffa14310581ab0b0908d0d7aecaefd453a89
zeljko <zeljko@z-ware.fi>
2015-10-10 19:15:38 +0300

    seed users: admin, normal and an app
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,10 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  # application specific fields
+  field :admin, type: Boolean, default: false
+  field :app, type: Boolean, default: false
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
 Mongo::Logger.logger.level = 1
+
+User.delete_all
+
+User.create! email: 'rocky@zwr.fi', password: 'balboa123', admin: true
+User.create! email: 'oscar@zwr.fi', password: 'balboa123'
+User.create! email: 'rck_prod_man@zwr.fi', password: '22#a0]uT)7#"1at', app: true
