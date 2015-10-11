58e378f1afc7deed1a7639947727d66d63b7f78e
zeljko <zeljko@z-ware.fi>
2015-10-11 20:23:38 +0300

    fixed request specs
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- spec/requests/apps_spec.rb
+++ spec/requests/apps_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Apps', type: :request do
   describe 'GET /apps' do
     it 'works! (now write some real specs)' do
-      get apps_path
+      @user = FactoryGirl.create(:admin)
+      post_via_redirect user_session_path,
+                        email: @user.email,
+                        password: @user.password,
+                        password_confirmation: @user.password
+      expect(response).to have_http_status(200)
+      get login_path
       expect(response).to have_http_status(200)
     end
   end
